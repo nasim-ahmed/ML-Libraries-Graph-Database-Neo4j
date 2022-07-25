b0e0eee6b96d9b03a2f802af8194b49d029fe48a
nasim <nasim.ahmed54@gmail.com>
2022-07-25 13:54:04 +0200

    added disgnosis

--- PyG-Neo4j/thesis/preprocessing/data_fetch.py
+++ PyG-Neo4j/thesis/preprocessing/data_fetch.py
@@ -11,17 +11,55 @@ class GraphFetcher(object):
         with open(self.configuration, 'r') as config_file:
             config = yaml.load(config_file, Loader = yaml.FullLoader)
 
+    def fetch_labels(self, connection): 
+        query = '''
+            call {
+                match(p:patientunitstay)-[:HAS_RESULT]->(apr:apachepatientresult)
+                where apr.apacheversion = 'IVa' and apr.actualiculos >= 1
+                with p.uniquepid as uniquepid, min(p.patienthealthsystemstayid) as patienthealthsystemstayid
+                match(p:patientunitstay) 
+                where p.patienthealthsystemstayid = patienthealthsystemstayid
+                with p.uniquepid as uniquepid, patienthealthsystemstayid, min(p.unitvisitnumber) as unitvisitnumber
+                return  patienthealthsystemstayid, unitvisitnumber
+            }
+            match(p:patientunitstay)-[:HAS_RESULT]->(apr:apachepatientresult)
+            where apr.apacheversion = 'IVa' and apr.actualiculos >= 1
+            and p.patienthealthsystemstayid = patienthealthsystemstayid
+            and p.unitvisitnumber = unitvisitnumber
+            return p.patientunitstayid AS patientunitstayid, apr.predictedhospitalmortality AS predictedhospitalmortality,
+            apr.actualhospitalmortality AS actualhospitalmortality, apr.predictediculos AS predictediculos, apr.actualiculos AS actualiculos 
+        '''
+
+        labels_df = connection.query(query)
+        labels_df.to_csv (r'labels.csv', index = False, header=True)
+
+
+    def fetch_diagnosis(self, connection): 
+        df = pd.read_csv('labels.csv')
+        col_one_list = df['patientunitstayid'].tolist()
+        query = '''
+        MATCH (p:patientunitstay) -[:HAS_DIAGNOSIS] -> (d: diagnosis)
+        WHERE d.patientunitstayid IN $col_one_list AND d.diagnosisoffset < 1440
+        return d.patientunitstayid AS patientunitstayid, d.diagnosisstring AS diagnosisstring
+        '''
+
+        result = connection.query(query, {"col_one_list": col_one_list})
+        print(result['patientunitstayid'].nunique())
+
+
+       
+
 
 def main():
     configure = 'config.yaml'
     graphFetcher = GraphFetcher(configure)
     graphFetcher.load_config()
     connection = Neo4jConnection(config)
-    query = '''
-      MATCH (n:DRUGS) RETURN n.name LIMIT 25
-    '''
-    df = connection.query(query)
-    print(df)
+
+    '''Fetch the labels table'''
+    #graphFetcher.fetch_labels(connection)
+    graphFetcher.fetch_diagnosis(connection)
+    
 
     
 
